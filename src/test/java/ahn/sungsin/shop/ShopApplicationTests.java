97dae3f37a19ef0752f290b4ed11a76e8c44a140
AHN <ahn01045503326@gmail.com>
2023-05-20 22:23:32 +0900

    initial project

---
+++ src/test/java/ahn/sungsin/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ahn.sungsin.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
